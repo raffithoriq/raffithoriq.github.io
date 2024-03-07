47b151ce3e190238b7a523052bd3a2f3e0387e75
raffithoriq <126893861+raffithoriq@users.noreply.github.com>
2024-03-07 21:29:08 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// toggel responsive
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener("click", () =>  {
+        navList.classList.toggle("nav-active")
+        burger.classList.toggle("toggle-burger")
+    })
+}
+
+navSlide();
+
+// clear
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+}
